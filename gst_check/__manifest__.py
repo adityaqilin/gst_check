fa61ceba1b662a2e2eea4ca36b518c8509528d61
Aditya-PC\Aditya <aditya@navya.co>
2018-04-24 19:51:01 +0530

    Added media files and HTML description

--- gst_check/__manifest__.py
+++ gst_check/__manifest__.py
@@ -8,21 +8,16 @@
     'description': """
         Performs the following checks when you enter a GSTIN number :
 			1. Length
-            2. Format 
+            2. Format
             3. Checksum
-		
 		It will give you a simple warning if it finds a wrong entry.
     """,
 
-    'author': "Aditya Agarwal",
+    'author': "Qilin Lab",
     'website': "http://www.qilinlab.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
-
+    'version': '1.0',
+    'images': ['images/main_1.png','images/main_screenshot.png'],
     # any module necessary for this one to work correctly
     'depends': ['base'],
 }
